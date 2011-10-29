fdd98763016217fe9c17771285ec86e93598e37d
Patrick Hanevold <patrick.hanevold@gmail.com>
2011-10-30 01:31:01 +0200

    cleanup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
   	@albums = Album.all
+  	@album = @albums.first
+  	@photo = @albums.first.photos.first
   end
 
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -7,24 +7,4 @@ class Photo
   field  :content, :type => String
   mount_uploader :image, ImageUploader
   embedded_in :album, :inverse_of => :photos
-
-  def photos
-    @photos ||= album.photos.all
-  end
-
-  def index
-    index = nil
-    photos.each_with_index do |photo,i|
-      index = i if photo.id == id
-    end
-    index
-  end
-
-  def next
-    photos[(index+1)%photos.size]
-  end
-
-  def previous
-    photos[(index-1)%photos.size]
-  end
 end
